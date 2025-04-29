550c4ed3eba62fb4e8624dcceb9534f505c0c834
IngGan1 <a55721948@gmail.com>
2025-04-29 10:48:12 +0900

    변수오류 해결

--- src/components/BookSearch.jsx
+++ src/components/BookSearch.jsx
@@ -43,6 +43,7 @@ function BookSearch() {
 
           // Supabase에 저장하고 싶으면 여기서 저장할 수 있음
           // 예시로 첫 번째 결과만 저장한다고 해봄
+          const firstBook = result.data.documents[0];
           const { data, error } = await supabase
             .from('Robot_Table') // 테이블명
             .insert([{ 
