1b8c3e3fa70487f5d5cc2b92e121fda2186ac013
IngGan1 <a55721948@gmail.com>
2025-04-30 15:10:27 +0900

    진짜아아아 db 수정

--- src/pages/BookDetail.jsx
+++ src/pages/BookDetail.jsx
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Supabase 세팅
 const supabaseUrl = 'https://gpciixncyvljjqorxqea.supabase.co';
-const supabaseKey = 'your_supabase_key'; // ⚠️ 실제 키로 유지
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwY2lpeG5jeXZsampxb3J4cWVhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDIyMzkwNywiZXhwIjoyMDU5Nzk5OTA3fQ.Jj1JmWWjdERE2lqjyqNhdYPAh3rHqTXcYv1fW3mBp9g'; // ⚠️ 실제 키로 유지
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 function BookDetail() {
