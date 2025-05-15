30dffcbe73bdc858a0db9fea28454aced281eafc
IngGan1 <a55721948@gmail.com>
2025-05-15 11:14:28 +0900

    임포트 넣음

--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -1,3 +1,6 @@
+import React, { createContext, useState, useContext } from 'react';
+import { supabase } from './supabaseClient';
+
 const ApiContext = createContext();
 
 export const ApiProvider = ({ children }) => {
