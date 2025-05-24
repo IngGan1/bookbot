9cb69dede2f374ae38774b22581b3b70df4b82be
IngGan1 <a55721948@gmail.com>
2025-05-24 20:42:23 +0900

    재수정

--- src/pages/BookDetail.jsx
+++ src/pages/BookDetail.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { supabase } from './context/supabaseClient'; // 클라이언트 import
+import { supabase } from '../context/supabaseClient'; // 클라이언트 import
 
 function BookDetail() {
   const location = useLocation();
